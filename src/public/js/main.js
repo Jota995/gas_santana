714f364801bcdd6f88ca7cd501b94fa5af85f84a
jota <jose.molina.castro18@gmail.com>
2020-08-17 23:52:28 -0400

    vista login y cambios a las vistas existentes

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,12 +1,50 @@
 let tab_container = Array.prototype.slice.apply(document.querySelectorAll('.tab'));
-let containers = Array.prototype.slice.apply(document.querySelectorAll('.container'));
+let containers = Array.prototype.slice.apply(document.querySelectorAll('.cards-container'));
+let date = document.querySelector('.fecha');
+
+let icons_tab = Array.prototype.slice.apply(document.querySelectorAll('.icon-tab'));
+let name_tab = Array.prototype.slice.apply(document.querySelectorAll('.name-tab'));
 
 document.querySelector('.tabs').addEventListener('click', event => {
     if (event.target.classList.contains('tab')) {
         let i = tab_container.indexOf(event.target);
-        tab_container.map(tab => tab.classList.remove('active'));
-        tab_container[i].classList.add('active');
-        containers.map(cont => cont.classList.remove('active-container'));
-        containers[i].classList.add('active-container');
+        tab_container.map(tab => tab.classList.remove('tab-active'));
+        tab_container[i].classList.add('tab-active');
+        containers.map(cont => cont.classList.remove('card-container-active'));
+        containers[i].classList.add('card-container-active');
+
+        if(i === 1){
+            date.classList.add("fecha-active");
+        }else{
+            date.classList.remove("fecha-active");
+        }
+    }
+    if (event.target.classList.contains('icon-tab')){
+        let i = icons_tab.indexOf(event.target);
+        tab_container.map(tab => tab.classList.remove('tab-active'));
+        tab_container[i].classList.add('tab-active');
+        containers.map(cont => cont.classList.remove('card-container-active'));
+        containers[i].classList.add('card-container-active');
+
+        if(i === 1){
+            date.classList.add("fecha-active");
+        }else{
+            date.classList.remove("fecha-active");
+        }
+        
+    }
+    if (event.target.classList.contains('name-tab')){
+        let i = name_tab.indexOf(event.target);
+        tab_container.map(tab => tab.classList.remove('tab-active'));
+        tab_container[i].classList.add('tab-active');
+        containers.map(cont => cont.classList.remove('card-container-active'));
+        containers[i].classList.add('card-container-active');
+
+        if(i === 1){
+            date.classList.add("fecha-active");
+        }else{
+            date.classList.remove("fecha-active");
+        }
+
     }
 })
